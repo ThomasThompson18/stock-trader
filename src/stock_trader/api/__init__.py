d69ccced1e43823770ef5eceaa96d7a1ca75cc8e
Thomas Thompson <Thomas.N.Thompson-1@ou.edu>
2022-12-21 15:48:11 -0600

    ready for first test run

--- src/stock_trader/api/__init__.py
+++ src/stock_trader/api/__init__.py
@@ -4,7 +4,9 @@ from flask import Blueprint
 
 flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 
-from flask_restx import Api  # noqa: E402
+from flask_restx import Api
+
+from stock_trader.api.trade.endpoints import trade_ns
 
 api_bp = Blueprint("api", __name__, url_prefix="/api/v1")
 authorizations = {"Bearer": {"type": "apiKey", "in": "header", "name": "Authorization"}}
@@ -17,3 +19,5 @@ api = Api(
     doc="/ui",
     authorizations=authorizations,
 )
+
+api.add_namespace(trade_ns, path="/trade")
